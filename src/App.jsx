5504011b2134bb8ec470105d8c9776293872e7d5
Moriya <moriyakatsman@hilma.tech>
2024-11-04 10:18:33 +0200

    leader board

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import LogIn from './LogIn';
+import LeaderBoard from './LeaderBoard';
 
 function App() {
 
   return (
     <>
       <LogIn />
+    {/* <LeaderBoard /> */}
     </>
   )
 }
---
+++ src/LeaderBoard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+function LeaderBoard() {
+    const users = JSON.parse(localStorage.getItem("users"));
+    
+    for (let i = 0; i < users.length; i++){
+        let total=0;
+        for (let j = 0; j <users[i].scores.length; j++){
+            total+=users[i].scores[j]
+        }
+        users[i].averageScore=Math.floor(total/users[i].scores.length);
+    //     console.log('users[i].userName: ', users[i].userName);
+    //     console.log('users[i].averageScore: ', users[i].averageScore);
+    }
+    let arr=users.sort((a,b)=>b.averageScore-a.averageScore)
+    const [topPlayers, setTopPlayers] = useState(arr)
+    // console.log('users: ', users);
+    // console.log('arr: ', arr);
+
+        return (
+            <>
+                <h1>Top players</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>User Name</th>
+                        <th>Average</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                   {topPlayers.map((player,i)=>{
+                    return(
+                        <tr key={i}>
+                        <td>{player.userName}</td>
+                        <td>{player.averageScore}</td>
+                    </tr>
+                    )
+                   })}
+                
+                    </tbody>
+
+                </table>
+            </>
+        )
+}
+export default LeaderBoard;
